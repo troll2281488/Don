38a4866f7f84a8fc98d2b22ac63511f3adae9afa
troll2281488 <sosunovdima92@gmail.com>
2026-05-18 00:05:51 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'donorgram-v1';
+const urlsToCache = [
+  '/Dono/',
+  '/Dono/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
